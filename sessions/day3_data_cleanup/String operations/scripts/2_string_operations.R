3b7899e37555c3b717f4b082d7b2c9e3576deba9
SozArmin <44461507+SozArmin@users.noreply.github.com>
2023-07-05 11:10:37 +0200

    day 3 exercise string operations

---
+++ sessions/day3_data_cleanup/String operations/scripts/2_string_operations.R
@@ -0,0 +1,27 @@
+# SICSS Berlin - Day 3 - July 5, 2023
+# String Operations - Exercise
+
+library(tidyverse)
+library(stringr)
+
+text <- "   I hope you will have a great time during the 9 days of SICSS-Berlin.  Orange  \nIf you have any suggestions for impovemend, feel free to tell us in person or via sicss.2023@wzb.eu.    Orange "
+
+### Use stringr functions, to solve the following tasks.
+
+# Is there a line break present? Show using a function
+
+# Remove the line break
+
+# Remove the two "Orange"
+
+# Remove the unnecessary white spaces
+
+# How long is this string? Count characters and words.
+
+# Extract the email address
+
+# Change the text to all lower-case
+
+# Correct the misspelled word
+
+
