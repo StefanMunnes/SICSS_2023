b08acaa8317dd4080ec914600415a5925abf2db2
Nini-Petrias <90155384+qm2021-npetrias@users.noreply.github.com>
2023-07-06 14:59:41 +0200

    np

--- exercises/NP/D4/day4_text_analysis_1.R
+++ exercises/NP/D4/day4_text_analysis_1.R
@@ -5,10 +5,228 @@ pacman::p_load(
   "quanteda.textplots", "quanteda.sentiment", "seededlda"
 )
 
+library(dplyr)
+library(tidyverse)
+library(stringr)
+library(lubridate)
+library(ggplot2)
 
 # load text data
-textdata <- readRDS("exercises/NP/CNN_complete.Rds")
+textdata_cnn <- readRDS("data/CNN_complete.Rds")
+textdata_fox <- readRDS("data/FOX_complete.Rds")
 
+#correct dates before merging
+#date variable
+print(textdata_fox$timestamp)
+print(textdata_cnn$timestamp)
+regex <- "\\n.*(Updated|Published)\\n.*,\\s([A-Z][a-z][a-z])\\s(.*)\\n\\s+"
+
+textdata_cnn <- textdata_cnn %>%
+  tidyr::extract(col = timestamp, 
+                 into = c("updated", "weekday", "date"), 
+                 regex = regex,
+                 remove = TRUE)
+
+# Alternative using stringr
+textdata_cnn <- textdata_cnn %>%
+  mutate(updated = str_extract(
+    timestamp,
+    regex,
+    group = 1),
+    weekday =  str_extract(
+      timestamp,
+      regex,
+      group = 2),
+    date = str_extract(
+      timestamp,
+      regex,
+      group = 3)) 
+
+# Date format
+textdata_cnn <- textdata_cnn %>%
+  mutate(date = parse_date(date, "%B %d, %Y",
+                           locale = locale("en")))
+
+#dates for fox news
+textdata_fox$timestamp <- gsub("\\s+", " ", str_trim(textdata_fox$timestamp))
+
+textdata_fox <- str_split_fixed(textdata_fox$timestamp, ",", 3) %>% 
+  data.frame() %>% 
+  rename(date = X1, year = X2) %>% 
+  cbind(textdata_fox, .)
+
+textdata_fox <- str_split_fixed(textdata_fox$hour, " ", 3) %>% 
+  data.frame() %>% 
+  rename(year = X1, hour = X2) %>% 
+  cbind(textdata_fox, .)
+
+textdata_fox$date <- paste(textdata_fox$year, textdata_fox$hour)
+
+#turn into a date variable
+textdata_fox$date2 <- as.Date(textdata_fox$date,
+                              format = "%B %d %Y")
+
+textdata_fox <- textdata_fox[,-7:-9]
+
+colnames(textdata_fox)[7] <- "date"
+
+names(textdata_fox)
+
+textdata_cnn <- textdata_cnn[,-3:-4]
+textdata_fox <- textdata_fox[,-6]
+textdata_fox <- textdata_fox[,c(1,2,6,3,4,5)]
+
+textdata <- rbind(textdata_cnn, textdata_fox)
+
+textdata2 <- rbind(textdata_cnn[1:100,], textdata_fox[1:100,])
+
+
+#TEXT ANALYSIS PART
+#1. load and inspect the whole corpus (documents, dimensions, tokens, types)
+#for Fox and CNN separat
+corpus <- corpus( textdata2,
+  docid_field = "url",
+  text_field = "body")
+
+head(summary(corpus)) 
+
+tokens <- tokens(corpus) # easy tokens without pre-processing
+head(tokens)
+
+dfm <- dfm(tokens) # DFM, without pre-processing
+head(dfm)
+dim(dfm)
+
+#2. use different pre-processing strategies and compare results
+topfeatures(dfm, 10)
+textstat_frequency(dfm, n = 10)
+
+#remove stopwords, symbols and extra characters? (I think, not sure)
+stopwords("en")
+
+tokens_pp <- tokens(
+  corpus,
+  remove_punct = TRUE,
+  remove_symbols = TRUE,
+  remove_numbers = TRUE,
+  remove_separators = TRUE
+) |>
+  tokens_tolower() |>
+  tokens_remove(stopwords("en"), padding = TRUE) |> #padding means remove the tokens and enters just the empty place holder
+  tokens_wordstem(language = "en")
+
+dfm_pp <- dfm(tokens_pp)
+head(dfm_pp)
+dim(dfm_pp)
+
+#this code trims the document-feature matrix dfm_pp by removing terms that
+#have a frequency less than 2, based on the count of occurrences. 
+#Then it calculates and returns the dimensions of the resulting matrix
+dfm_trim(dfm_pp, min_termfreq = 2, termfreq_type = "count") |>
+  dim()
+
+#3. show most important compound and co-occuring words
+# keywords, most frequent words
+textstat_frequency(dfm_pp, n = 15)
+
+#for co-occurence
+textstat_collocations(tokens_pp) |>
+  head(15)
+
+
+fcm_pp <- fcm(tokens_pp, context = "window", count = "frequency", window = 3)
+dim(fcm_pp)
+
+topfeatures(fcm_pp)
+
+fcm_pp_subset <- fcm_select(fcm_pp, names(topfeatures(fcm_pp, 40)))
+
+textplot_network(fcm_pp_subset)
+
+#4. extract important keywords
+# keywords, most frequent words
+textstat_frequency(dfm_pp, n = 15)
+
+# TF-IDF
+tfifdf_pp <- dfm_tfidf(dfm_pp)
+
+topfeatures(tfifdf_pp, n = 5, groups = docnames(tfifdf_pp))
+topfeatures(dfm_pp, n = 5, groups = docnames(dfm_pp))
+
+# keyness
+textstat_keyness(dfm, target = 1:5) |> # docvars(dfm, "article_length") < 4000
+  textplot_keyness()
+
+convert(fcm_pp, to = "data.frame") |>
+  dplyr::select(doc_id, "twitter") |>
+  dplyr::filter(twitter > 0) |>
+  dplyr::arrange(desc(twitter))
+
+
+#5- over time
+corpus <- corpus(textdata2,
+                  docid_field = "url",
+                  text_field = "body")
+head(summary(corpus)) 
+
+
+
+
+#dictionary part 1
+#preparing the data
+corpus <- corpus( textdata2,
+                  docid_field = "url",
+                  text_field = "body")
+
+head(summary(corpus)) 
+
+tokens <- tokens(corpus) # easy tokens without pre-processing
+head(tokens)
+
+dfm <- dfm(tokens) # DFM, without pre-processing
+head(dfm)
+dim(dfm)
+
+#remove stopwords, symbols and extra characters? (I think, not sure)
+stopwords("en")
+
+tokens_pp <- tokens(
+  corpus,
+  remove_punct = TRUE,
+  remove_symbols = TRUE,
+  remove_numbers = TRUE,
+  remove_separators = TRUE
+) |>
+  tokens_tolower() |>
+  tokens_remove(stopwords("en"), padding = TRUE) |> #padding means remove the tokens and enters just the empty place holder
+  tokens_wordstem(language = "en")
+
+dfm_pp <- dfm(tokens_pp)
+head(dfm_pp)
+dim(dfm_pp)
+
+#this code trims the document-feature matrix dfm_pp by removing terms that
+#have a frequency less than 2, based on the count of occurrences. 
+#Then it calculates and returns the dimensions of the resulting matrix
+dfm_trim(dfm_pp, min_termfreq = 2, termfreq_type = "count")
+
+#apply dictionary
+dict_pol <- data_dictionary_HuLiu
+
+dfm_lookup(dfm_pp, dict_pol) #negative, positive and neutral dictionaries
+textstat_polarity(dfm_pp, dict_pol)
+
+sent_score <- textstat_valence(dfm_pp, data_dictionary_AFINN) #how different the words on the positive and negative scales are scaled
+colnames(sent_score)[1] <- "url"
+textdata2 <- merge(textdata2, sent_score, by="url")
+
+
+
+
+
+
+
+#######stefan's code
 
 # create corpus, tokens and dfm (just first ten documents)
 corpus <- corpus(textdata[1:100, ], text_field = "body") #this takes first 100 rows of text
